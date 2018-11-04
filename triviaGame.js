dd072af319b4dbca375e951380dc3ea71ae8b9a4
unknown <thoneywill3@gmail.com>
2018-11-03 22:28:14 -0500

    First commit

---
+++ triviaGame.js
@@ -0,0 +1,10 @@
+const wordList =  ['word1', 'word2', 'word3'];
+
+let randNum = math.floor(math.random() * word.length);
+let chosenWord = word[randNum];
+let rightWord = [];
+let wrongWord = [];
+let underScore = [];
+
+// Dom manipulation
+
